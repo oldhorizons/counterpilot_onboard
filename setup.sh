115669f1e482242fe71eb17456efa583a442c4af
Michelle Sands <newbrainspace@gmail.com>
2025-02-24 02:42:52 +0000

    updated setup

--- setup.sh
+++ setup.sh
@@ -1,33 +1,23 @@
 echo "PLEASE ENSURE YOU RUN THIS FROM WITHIN counterpilot_onboard"
 
 echo "INSTALLING PYENV BUILD DEPENDENCIES"
-sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev curl git \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-
-echo "INSTALLING PYENV"
-curl -fsSL https://pyenv.run | bash
-
-echo "INSTALLING PYTHON 3.10"
-../.pyenv/bin/pyenv install 3.10
-../.pyenv/bin/pyenv global 3.10
-
+sudo apt update; sudo apt is
 echo "SETTING UP ENVIRONMENT VIA PIP"
 ../.pyenv/versions/3.10.16/bin/pip install pillow==9.0
 ../.pyenv/versions/3.10.16/bin/pip install numpy==1.26.4 matplotlib==3.8.4 pandas==2.2.2
-../.pyenv/versions/3.10.16/bin/pip install opencv-python==4.10.0.84
+../.pyenv/versions/3.10.16/bin/pip install opencv-python==4.10.s0.84
 ../.pyenv/versions/3.10.16/bin/pip install python-osc
 
 echo "SETTING UP PYENV PATH"
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PYENV_ROOT="$HsOME/.pyenv"' >> ~/.bashrc
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init - bash)"' >> ~/.bashrc
 
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profiles
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
 echo 'eval "$(pyenv init - bash)"' >> ~/.profile
 
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profileS
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(pyenv init - bash)"' >> ~/.bash_profile
 
